9e2d68fd0db3c5709f971702e9d284a818ff510c
Charles_CH.HO <34810665+a20041060@users.noreply.github.com>
2026-05-16 17:47:26 +0800

    Add VPC and public subnet resources to network module

--- terraform-masterclass/02-modular-network/modules/network/main.tf
+++ terraform-masterclass/02-modular-network/modules/network/main.tf
@@ -1,1 +1,13 @@
-# Network module
+resource "aws_vpc" "main" {
+  cidr_block = var.vpc_cidr
+  tags       = { Name = "${var.env_name}-vpc" }
+}
+
+resource "aws_subnet" "public" {
+  vpc_id     = aws_vpc.main.id
+  cidr_block = cidrsubnet(var.vpc_cidr, 8, 1) # Automatically calculates 10.x.1.0/24 splits
+  tags       = { Name = "${var.env_name}-public-subnet" }
+}
+
+output "vpc_id"    { value = aws_vpc.main.id }
+output "subnet_id" { value = aws_subnet.public.id }
